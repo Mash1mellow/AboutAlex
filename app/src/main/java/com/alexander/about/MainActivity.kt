b2e93bafa079919460c06c31d1ac99aa915e9b02
黃植達 <alexander.rolind@gmail.com>
2023-03-22 14:34:16 +0800

    v.2(黃植達)

--- app/src/main/java/com/alexander/about/MainActivity.kt
+++ app/src/main/java/com/alexander/about/MainActivity.kt
@@ -15,7 +15,7 @@ class MainActivity : AppCompatActivity(){
         txv.text = "我是黃植達"
         txv.setOnClickListener({
             if (txv.text == "黃植達你好"){
-                txv.text = "數媒系不錯"
+                txv.text = "海青班很棒,數媒更是好的方向"
             }
             else {
                 txv.text = "黃植達你好"
